5cfb90d0f1feefe6cd6eacbc3d460d6a117a25c2
Kieran Colford <kieran@kcolford.com>
2017-10-24 16:18:23 -0400

    added Parallel function

--- builtins.go
+++ builtins.go
@@ -1,6 +1,7 @@
 package builtins
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"runtime"
@@ -20,10 +21,18 @@ func Ignore(err error) {
 	}
 }
 
+type Panic struct {
+	Recovered interface{}
+}
+
+func (p Panic) Error() string {
+	return fmt.Sprintf("panic: %s", p.Recovered)
+}
+
 func Recover() error {
 	err := recover()
 	if err != nil {
-		return fmt.Errorf("panic: %s", err)
+		return Panic{err}
 	}
 	return nil
 }
@@ -37,4 +46,34 @@ func End() {
 			time.Sleep(100000 * time.Hour)
 		}
 	}
+}
+
+func Parallel(ctx context.Context, fns ...func(context.Context) error) (err error) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	ch := make(chan error, len(fns))
+	for _, fn := range fns {
+		fn := fn
+		Go(func() {
+			ch <- func() (e error) {
+				defer func() {
+					err := Recover()
+					if err != nil {
+						e = err
+					}
+				}()
+				e = fn(ctx)
+				return
+			}()
+		})
+	}
+	for _ = range fns {
+		err = <-ch
+		if err != nil {
+			return
+		}
+	}
+
+	return
 }
